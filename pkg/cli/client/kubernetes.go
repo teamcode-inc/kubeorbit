9fb34a995c504c49c56f9265af9ff3798751f506
Eric <eric.shen@vibin.it>
2022-02-24 21:04:33 +0800

    fix #7

--- pkg/cli/client/kubernetes.go
+++ pkg/cli/client/kubernetes.go
@@ -17,6 +17,10 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	log "kubeorbit.io/pkg/cli/logger"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 var kubeConfig *rest.Config
